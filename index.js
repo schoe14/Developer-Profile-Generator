d3f6f50738c098d98cb7e4085a6fd347296d5df0
Seohui Choe <chelsea.choe@gmail.com>
2020-01-18 22:16:53 -0500

    fixed formating issue and broke down functions

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const path = require('path');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const compile = async function(templateName, data){
+const compile = async function (templateName, data) {
     const filePath = path.join(process.cwd(), 'templates', `${templateName}.hbs`);
-    const html = await readFileAsync (
+    const html = await readFileAsync(
         filePath,
         "utf8"
     );
@@ -33,57 +33,71 @@ function promptUser() {
     ]);
 }
 
+async function getGithubData() {
+    const answers = await promptUser();
+
+    const { data } = await axios.get(
+        `https://api.github.com/users/${answers.userName}?per_page=100`
+    );
+
+    const reposData = await axios.get(
+        `https://api.github.com/users/${answers.userName}/repos?per_page=100`
+    );
+
+    let starsCount = 0;
+    reposData.data.map(function (res) {
+        return starsCount += res.stargazers_count;
+    });
+
+    const locationSearch = () => {
+        if (data.location != null && data.location.includes(" ")) {
+            return data.location.split(" ")[0] + "%20" + data.location.split(" ")[1];
+        }
+        else return data.location;
+    }
+
+    const infoObj = {
+        login: data.login, profileImg: data.avatar_url, name: data.name,
+        location: data.location, locationLink: `https://www.google.com/maps/place/${locationSearch()}`, githubProfile: data.html_url, blog: data.blog, bio: data.bio,
+        publicRepos: data.public_repos, followers: data.followers, githubStars: starsCount,
+        following: data.following, favoriteColor: answers.color
+    };
+
+    await writeFileAsync(
+        "developer-data.json",
+        JSON.stringify(infoObj, null, 2),
+        "utf8"
+    );
+
+    if (infoObj.name == null) return infoObj.login;
+    else return infoObj.name;
+}
+
 (async function () {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        // -----------------------------------------------------------------------------------
-        const answers = await promptUser();
 
-        const { data } = await axios.get(
-            `https://api.github.com/users/${answers.userName}?per_page=100`
-        );
-
-        const reposData = await axios.get(
-            `https://api.github.com/users/${answers.userName}/repos?per_page=100`
-        );
-
-        let starsCount = 0;
-        reposData.data.map(function (res) {
-            return starsCount += res.stargazers_count;
-        });
-        // -------------------------------------------------------------------------------------
-        const locationArr = data.location.split(", ");
-        const infoObj = {
-            profileImg: data.avatar_url, name: data.name,
-            location: data.location, locationLink: `https://www.google.com/maps/place/${data.location}`, githubProfile: data.html_url, blog: data.blog, bio: data.bio,
-            publicRepos: data.public_repos, followers: data.followers, githubStars: starsCount,
-            following: data.following, favoriteColor: answers.color
-        };
-
-        await writeFileAsync(
-            "developer-data.json",
-            JSON.stringify(infoObj, null, 2),
-            "utf8"
-        );
+        const pdfName = await getGithubData();
 
         const devData = await require('./developer-data.json');
-        console.log(devData);
+        // console.log(devData);
 
         const content = await compile('pdf-template', devData);
-        console.log(content);
+        // console.log(content);
 
         await page.setContent(
             content
         );
         await page.emulateMedia('screen');
+
         await page.pdf({
-            path: `./dev-pdf-generated/${infoObj.name}.pdf`,
+            path: `./dev-pdf-generated/${pdfName}.pdf`,
             format: 'A4',
             printBackground: true
         });
 
-        console.log('Successfully generated pdf!');
+        console.log(`Successfully generated ${pdfName}.pdf!`);
 
         await browser.close();
         process.exit();
